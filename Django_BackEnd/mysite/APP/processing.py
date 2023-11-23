404a2c950d233d9f909329d126abfb73e95557a9
NoMamesWey1 <josejimenez530@yahoo.com>
2023-11-22 22:41:10 -0800

    updating processing.py

--- Django_BackEnd/mysite/APP/processing.py
+++ Django_BackEnd/mysite/APP/processing.py
@@ -3,21 +3,6 @@ import re
 from datetime import datetime
 from pytz import timezone
 
-# def processTime():
-#     sl = ""
-#     lt = time.localtime()[:6]
-#     offset = abs(time.localtime().tm_gmtoff) / 3600
-#     if time.localtime().tm_gmtoff < 0: 
-#         sl = '-'
-#     else:
-#         sl = '+'
-#     # offsetHr, offsetMin =  f"{int(offset):02}", f"{int(offset % int(offset))*60:02}"
-#     offsetHr, offsetMin = f"{int(offset):02}", f"{int((offset % 1) * 60):02}"#this way for mac. does it work for windows?
-#     s = str(lt[0]) + '-' + str(f"{lt[1]:02}")+ '-' + str(f"{lt[2]:02}") +'T' +  str(f"{lt[3]:02}") + "%3A" + str(f"{lt[4]:02}") + "%3A" + str(lt[5]) + sl + offsetHr + "%3A" + offsetMin
-#     print(s)
-#     return s
-
-#dhruv 
 datetime_NewYork = datetime.now(timezone('America/Los_Angeles'))
 print("LA time:", datetime_NewYork.strftime("%H:%M:%S"))
 
