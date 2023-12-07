673ea57dc15c92dcba3bef27b9d3645fdf724db0
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2023-12-07 13:04:30 -0800

    dates

--- Django_BackEnd/mysite/APP/processing.py
+++ Django_BackEnd/mysite/APP/processing.py
@@ -7,9 +7,21 @@ datetime_NewYork = datetime.now(timezone('America/Los_Angeles'))
 print("LA time:", datetime_NewYork.strftime("%H:%M:%S"))
 
 
+# def processTime():
+#     s = time.asctime()
+#     return s
+
 def processTime():
-    s = time.asctime()
-    return s
+    def date(keyword):
+     return getattr(time.localtime(), keyword, None)
+ 
+    t = "year-month-dayThour%3Aminute%3Asecond-08%3A00"
+    b = ['year','month','day','hour','minute','second']
+    for i in range(len(b)):
+        d = str(date(str(time.localtime().__match_args__[i])))
+        t = t.replace(b[i],"0" + d if len(d) < 2 else d)
+        
+    return t
 
 def processingEventData(text):
 
